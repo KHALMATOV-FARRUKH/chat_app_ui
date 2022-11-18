3b910c64268bb549ed4e20d14d89b1e2c1066084
Fara <115084566+KHALMATOV-FARRUKH@users.noreply.github.com>
2022-11-18 09:50:17 +0500

    Added dependence: get: ^4.6.5
    Created folders:
    config
    models
    screens
    widgets
    
    Created dart files:
    theme.dart
    chat_model.dart
    message_model.dart
    models.dart
    user_model.dart
    chat_screen.dart
    home_screen.dart
    screens.dart
    custom_container.dart
    widgets.dart
    
    Added permission to manifest file.
    Path: android/app/src/main/AndroidManifest.xml
    Permission: <uses-permission android:name="android.permission.INTERNET"/>

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'package:chat_app_ui/config/theme.dart';
+import 'package:chat_app_ui/screens/chat_screen.dart';
+import 'package:chat_app_ui/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,15 +12,18 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Chat App UI',
+      theme: lightTheme(),
+      darkTheme: darkTheme(),
+      home: const HomeScreen(),
+      getPages: [
+        GetPage(name: '/', page: () => const HomeScreen()),
+        GetPage(name: '/chat', page: () => const ChatScreen()),
+      ],
     );
   }
 }
